a098cce96b86e9d01f0d79e7dc5c5921af090eba
Fatare <51864367+Fatare@users.noreply.github.com>
2021-12-20 18:13:20 +0200

    Add files via upload

---
+++ Project_App/Sprzedane produkty.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Project_App
+{
+    /// <summary>
+    /// Interaction logic for Sprzedane_produkty.xaml
+    /// </summary>
+    public partial class Sprzedane_produkty : Window
+    {
+        public Sprzedane_produkty()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Project_App/Tworzenia zadań.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Project_App
+{
+    /// <summary>
+    /// Interaction logic for Tworzenia_zadań.xaml
+    /// </summary>
+    public partial class Tworzenia_zadań : Window
+    {
+        public Tworzenia_zadań()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
